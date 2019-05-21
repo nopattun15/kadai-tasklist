65029dd8f10dd2198d2526236401483d92781dfa
patuo <nopattun15@gmail.com>
2019-05-21 10:52:36 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
@@ -6,4 +10,7 @@ Rails.application.routes.draw do
   post "login", to:"sessions#create"
   delete "logout", to:"sessions#destroy"
   resources :tasks
+  
+  get "signup", to:"users#new"
+  resources :users, only:[:index,:show,:new,:create]
 end
