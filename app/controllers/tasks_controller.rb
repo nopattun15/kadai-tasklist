3bfe3831db4abca3c9ec3fa929e9705446a88b53
patuo <nopattun15@gmail.com>
2019-05-25 07:51:33 +0000

    Task fix 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
     before_action :correct_user, only: [:show,:update,:edit,:destroy]
     
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(10)
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
     end 
     
     def show
